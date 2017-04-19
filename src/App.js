c85739155d00325b958d14e6954b6d3c90ffa56f
nilssonja <justin.a.nilsson@gmail.com>
2017-04-18 21:19:35 -0400

    Create basic aframe scene within App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'aframe';
 import './App.css';
 
 class App extends Component {
+  clickHandler(event) {
+    console.log('You clicked the box!', event);
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <a-scene stats>
+        <a-sky color="#FFF"/>
+        <a-plane color="black" onClick={this.clickHandler.bind(this)} rotation="-90 0 0" scale="100 100 0"/>
+        <a-camera cursor
+                  position="0 0 -1"
+                  geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
+                  material="color: black; shader: flat">
+        </a-camera>
+      </a-scene>
     );
   }
 }
