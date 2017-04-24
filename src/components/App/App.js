5c315c398958989fb01675b9eb61e91f9507575c
Yukiiro-Nite <e.kazuma@gmail.com>
2017-04-23 20:34:36 -0400

    Added Voxel.js and changed the file structure a bit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Voxel from '../Voxel/Voxel';
 import 'aframe';
 import './App.css';
 
@@ -12,6 +13,7 @@ class App extends Component {
       <a-scene stats>
         <a-sky color="#FFF"/>
         <a-plane color="black" onClick={this.clickHandler.bind(this)} rotation="-90 0 0" scale="100 100 0"/>
+        <Voxel />
         <a-camera cursor
                   position="0 0 -1"
                   geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
---
+++ src/components/Voxel/Voxel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import 'aframe';
+
+const Voxel = ( props ) =>
+  <a-box { ...props }/>;
+
+export default Voxel;
